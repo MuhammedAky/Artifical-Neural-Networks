8a9cb4ef9f19e7814a10e54ef1806996f376e225
MuhammedAky <muhammedaky24@gmail.com>
2022-11-02 22:57:40 +0300

    First commit

---
+++ include/utils/Math.hpp
@@ -0,0 +1,20 @@
+#ifndef _MATH_HPP_
+#define _MATH_HPP_
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <assert.h>
+#include "../Matrix.hpp"
+
+using namespace std;
+
+namespace uils {
+    class Math {
+        public:
+            static void multiplyMatrix(Matrix *a, Matrix *b, Matrix *c);
+    };
+}
+
+#endif
